262b68e4dbb3e5655af1aff1c997378cce0cf730
Aleksandr Medvedev <crekdoo@yandex.ru>
2023-09-20 22:42:15 +0300

    added handlers

--- cacao_bot.py
+++ cacao_bot.py
@@ -1,2 +1,20 @@
-print()
-print('Привет')
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+import os
+
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def command_start(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'Какао хочешь?')
+        await message.delete()
+    except:
+        await message.reply('Общение с ботом через ЛС, напишите ему: \nhttps://t.me/crekdoo_bot')
+
+
+
+executor.start_polling(dp, skip_updates=True)
