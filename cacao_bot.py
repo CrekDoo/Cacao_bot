a8a3896536ae31ae326de5b129f5ca2ed604a65d
Aleksandr Medvedev <crekdoo@yandex.ru>
2023-09-21 00:20:33 +0300

    Первая рабочая версия

--- cacao_bot.py
+++ cacao_bot.py
@@ -1,20 +1,14 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-
-import os
-
-bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher(bot)
-
-@dp.message_handler(commands=['start', 'help'])
-async def command_start(message: types.Message):
-    try:
-        await bot.send_message(message.from_user.id, 'Какао хочешь?')
-        await message.delete()
-    except:
-        await message.reply('Общение с ботом через ЛС, напишите ему: \nhttps://t.me/crekdoo_bot')
+from create_bot import dp
+from handlers import client, admin, other
 
 
+async def on_startup(_):
+    print('Полетели!!!')
 
-executor.start_polling(dp, skip_updates=True)
+client.register_handlers_client(dp)
+admin.register_handlers_admin(dp)
+other.register_handlers_other(dp)
+
+
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
