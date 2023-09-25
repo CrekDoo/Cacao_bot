c9992da5c567d835d52b5264f43d4b158f19830f
Aleksandr Medvedev <crekdoo@yandex.ru>
2023-09-26 00:13:54 +0300

    Добавил клавиатуру для админа

--- cacao_bot.py
+++ cacao_bot.py
@@ -1,10 +1,12 @@
 from aiogram.utils import executor
 from create_bot import dp
 from handlers import client, admin, other
-
+from data_base import sqlite_db
 
 async def on_startup(_):
     print('Полетели!!!')
+    sqlite_db.sql_start()
+
 
 client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
